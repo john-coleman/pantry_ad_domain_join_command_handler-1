b692c02fd5817341f1cb5a7c46e9aeea49c69bde
Clive Foley <clive.foley@wonga.com>
2013-08-12 16:58:00 +0100

    Creating initial repo

---
+++ pantry_ad_domain_join_command_handler/pantry_ad_domain_join_command_handler.rb
@@ -0,0 +1,13 @@
+module Wonga
+	module Daemons
+		class PantryAdDomainJoinCommandHandler
+			def initialize(config)
+				@config = config
+			end
+
+			def handle_message(message)
+				#Put message handling code in here
+			end
+		end
+	end
+end
---
+++ spec/pantry_ad_domain_join_command_handler/pantry_ad_domain_join_command_handler_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+require_relative '../../pantry_ad_domain_join_command_handler/pantry_ad_domain_join_command_handler'
+
+describe Wonga::Daemons::PantryAdDomainJoinCommandHandler do
+	describe "#handle_message" do
+		#add in unit test code for handle message here.
+	end
+end
+
